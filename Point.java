1b31c613e7fa790817caa5bbc0bd786f3cba6417
Camila Contreras <ccontreras20@stuy.edu>
2020-11-04 11:38:12 -0800

    points established

---
+++ Point.java
@@ -0,0 +1,26 @@
+public class Point {
+  private double x,y;
+  public Point(double X, double Y) {
+    x = X;
+    y = Y;
+  }
+  public Point(Point p) {
+    x = p.x;
+    y = p.y;
+  }
+  public double getX() {
+    return x;
+  }
+  public double getY() {
+    return y;
+  }
+  public double distanceTo(Point other) {
+    return (Math.sqrt(Math.pow(x-other.x,2) + Math.pow(y-other.y, 2)));
+  }
+  public boolean equals (Point other) {
+    return x == other.x && y == other.y;
+  }
+  public String toString() {
+    return "(" + x + "," + y + ")";
+  }
+}
